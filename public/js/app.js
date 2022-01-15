95d1f911a0df66b0f0a8ed0e838ee6919f0878e2
Salvador Vilca <salvasvip@gmail.com>
2022-01-14 22:07:04 -0400

    building product component

--- public/js/app.js
+++ public/js/app.js
@@ -1,74 +1,38 @@
 class ProductList extends React.Component {
-
-    handleProductUpVote(productId) {
-        console.log(productId + ' was upvoted.');
-    }
-
     render() {
-        const products = Seed.products.sort((a, b) => (
-            b.votes - a.votes
-        ));
-        const productComponents = products.map((product) => (
-            <Product
-                key={'product-' + product.id}
-                id={product.id}
-                title={product.title}
-                description={product.description}
-                url={product.url}
-                votes={product.votes}
-                submitterAvatarUrl={product.submitterAvatarUrl}
-                productImageUrl={product.productImageUrl}
-                onVote={this.handleProductUpVote}
-            />
-        ));
         return (
             <div className='ui unstackable items'>
-                {productComponents}
+                <Product />
             </div>
-        );
+        )
     }
 }
 
 class Product extends React.Component {
-    handleUpVote() {
-        this.props.onVote(this.props.id);
-    }
-
     render() {
-        return (
-            <div className='item'>
-                <div className='image'>
-                    <img src={this.props.productImageUrl}/>
+        return(
+            <div className="item">
+                <div className="image">
+                    <img src="images/products/image-aqua.png" />
                 </div>
-                <div className='middle aligned content'>
-                    <div className='header'>
-                        <a onClick={this.handleUpVote}>
-                            <i className='large caret up icon'/>
-                        </a>
-                        {this.props.votes}
+                <div className="middle aligned content">
+                    <div className="description">
+                        <a>Fort Knight</a>
+                        <p>Authentic renaissance actors, delivered in just two week\s.</p>
                     </div>
-                    <div className='description'>
-                        <a href={this.props.url}>
-                            {this.props.title}
-                        </a>
-                        <p>
-                            {this.props.descraiption}
-                        </p>
-                    </div>
-                    <div className='extra'>
+                    <div className="extra">
                         <span>Submitted by:</span>
-                        <img
-                            className='ui avatar image'
-                            src={this.props.submitterAvatarUrl}
-                        />
+                        <img className="ui avatar image" src="images/avatars/daniel.jpg"/>
                     </div>
                 </div>
             </div>
-        );
+        )
     }
 }
 
 ReactDOM.render(
     <ProductList/>,
     document.getElementById('content')
-);
+)
+
+// we stay on page 53
